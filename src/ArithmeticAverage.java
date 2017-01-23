f8cf1a392cc5065c01139858539763fd4ed47c2c
Dan-gem <Dan_dan_gen@mail.ru>
2017-01-23 18:15:15 +0600

    Initial commit

---
+++ src/ArithmeticAverage.java
@@ -0,0 +1,12 @@
+public class ArithmeticAverage {
+    public static void main(String[] args) {
+        short a = 4, b = 6, c = 7;
+
+        float sa = (float) ( a + b + c) / 3;
+        System.out.println (sa);
+
+
+
+
+    }
+}
